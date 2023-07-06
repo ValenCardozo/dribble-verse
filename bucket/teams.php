6d9832ec94d7b3b1cd5616c5afe3a2b99a5c878a
ValenCardozo <valencardozo7@gmail.com>
2023-07-06 20:05:44 -0300

    first commit

---
+++ bucket/teams.php
@@ -0,0 +1,37 @@
+<?php
+
+// Scores: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard
+
+// News: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news
+
+// All Teams: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams
+
+// Specific Team: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/:team
+
+
+$ch = curl_init();
+
+curl_setopt_array($ch, [
+    CURLOPT_URL => "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams",
+    CURLOPT_RETURNTRANSFER => true,
+    // CURLOPT_HTTPHEADER => $headers,
+    // CURLOPT_HEADERFUNCTION => $header_callback
+]);
+
+$result = json_decode(curl_exec($ch));
+$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+curl_close($ch);
+
+$teams = $result->sports[0]->leagues[0]->teams;
+foreach ($teams as $info) {
+	$name = $info->team->displayName;
+	$logo = $info->team->logos[0]->href;
+
+	$show = '<img src="'. $logo .'" style="width:42px;height:42px;">';
+	echo '<pre>' . $show . $name . "\n";
+}
+
+// echo $status_code;
+// print_r($response_headers);
+// echo $response, "\n";
